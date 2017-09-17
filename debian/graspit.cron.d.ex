600f29282a6ded75fe69f0986bb2ebf01a92ba65
David Watkins <djrival7@gmail.com>
2017-09-17 15:28:14 -0400

    Removed superfluous debian files

--- debian/graspit.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the graspit package
-#
-0 4	* * *	root	[ -x /usr/bin/graspit_maintenance ] && /usr/bin/graspit_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(graspit):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="graspit" command="/usr/bin/graspit"
